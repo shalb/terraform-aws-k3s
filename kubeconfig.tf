95a090478e5be8997b5434e2bebaacdda3b473b5
romanprog <arti@shalb.com>
2021-04-28 18:15:42 +0000

    debug output

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "wait_cluster_ready" {
   provisioner "local-exec" {
-    command = "until (aws s3 cp s3://${var.s3_bucket}/${var.cluster_name}/${local.s3_kubeconfig_filename} ./kubeconfig_tmp && for i in $(seq 10); do kubectl version --kubeconfig ./kubeconfig_tmp --request-timeout=5s || exit 1; sleep 1; done) >/dev/null 2>&1; do sleep 1; echo waiting for kubeconfig; done"
+    command = "until (aws s3 cp s3://${var.s3_bucket}/${var.cluster_name}/${local.s3_kubeconfig_filename} ./kubeconfig_tmp && for i in $(seq 10); do kubectl version --kubeconfig ./kubeconfig_tmp --request-timeout=5s || exit 1; sleep 1; done); do sleep 1; echo waiting for kubeconfig; done"
   }
   depends_on = [
     aws_autoscaling_group.master,
